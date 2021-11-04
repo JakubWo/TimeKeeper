cd182919e9de01a7d35af6f4ee0df4f3549ff40c
JakubWo <tosiekandjojo@gmail.com>
2021-11-04 13:04:42 +0100

    Changes to routes, regarding htaccess.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include("src/Service/RoutingService/RoutingService.php");
 use src\Service\RoutingService\RoutingService;
 $routing = new RoutingService();
 
-
 if($_SERVER['REQUEST_URI'] === '/'){
     require($routing->getRoute('template-login'));
 } else {
--- src/Service/RoutingService/RoutingService.php
+++ src/Service/RoutingService/RoutingService.php
@@ -20,9 +20,16 @@ class RoutingService{
         if(empty($this->routes)) {
             $this->routes = yaml_parse_file("config/routes.yaml");
         }
-
         $path_parts = explode('-', $path_code);
         return $this->routes[$path_parts[0]][$path_parts[1]];
+    }
+
+    private function printRoutes(): void
+    {
+        foreach($this->routes as $route => $vals){
+            foreach($vals as $val)
+                echo "$route   $val<br>";
+        }
     }
 
 }
