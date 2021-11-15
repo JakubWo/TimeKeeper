65734cf021b790cc53c8827c85c251c6d514fd83
JakubWo <tosiekandjojo@gmail.com>
2021-11-15 19:28:12 +0100

    Half-responsive login sub-site

--- src/Template/login.php
+++ src/Template/login.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= $GLOBALS['routing']->getRoute('style-login') ?>">
-    <link rel="shortcut icon" href="<?= $GLOBALS['routing']->getRoute('image-favicon') ?>">
-    <title>Login</title>
-</head>
-<body>
-<div></div>
-    <form action="<?= $GLOBALS['routing']->getRoute('service-auth') ?>">
-        <button type="submit">
-            Log in
-        </button>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="<?= $GLOBALS['routing']->getRoute('style-login') ?>">
+        <link rel="shortcut icon" href="<?= $GLOBALS['routing']->getRoute('image-favicon') ?>">
+        <title>Login</title>
+    </head>
 
+    <body>
+    <div>
+        <form action="<?= $GLOBALS['routing']->getRoute('service-auth') ?>">
+            <label>
+                <p>Username:</p>
+                <input type="email" id="username_input">
+            </label>
 
-    </form>
+            <label>
+                <p>Password:</p>
+                <input type="password" id="password_input">
+            </label>
 
-</body>
+            <p><a href="">Terms of use</a></p>
+
+            <label>
+                <button type="submit" id="submit_button">Log in</button>
+            </label>
+        </form>
+    </div>
+    </body>
 </html>
